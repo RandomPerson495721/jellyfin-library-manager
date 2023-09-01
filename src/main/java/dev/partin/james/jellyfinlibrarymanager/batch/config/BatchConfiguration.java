24da0fe798381ad539443426ae4a8169f83611eb
James Partin <jamespartin@Jamess-MacBook-Pro-2.local>
2023-09-01 19:00:10 -0400

    .

--- src/main/java/dev/partin/james/jellyfinlibrarymanager/batch/config/BatchConfiguration.java
+++ src/main/java/dev/partin/james/jellyfinlibrarymanager/batch/config/BatchConfiguration.java
@@ -3,7 +3,12 @@ package dev.partin.james.jellyfinlibrarymanager.batch.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.Reader;
+
 @Configuration
 public class BatchConfiguration {
+    @Bean
+    public Reader japReader() {
 
+    }
 }
